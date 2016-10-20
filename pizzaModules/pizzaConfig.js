bc58fc13cc9bd6bb70bf8287efda6292b9e0803b
Benjamin Auzanneau (necraidan) <benjamin.auzanneau.tm@gmail.com>
2016-10-20 16:41:45 +0200

    Coucou

--- pizzaModules/pizzaConfig.js
+++ pizzaModules/pizzaConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   login: 'pizza.js',
-  mdp: '',
+  mdp: '?Marguar1taOrCalzon3!',
   idEquipe: '',
   idPartie: '',
-  battleUrl: 'http://www.---.date/battle-ws/duel/',
+  battleUrl: 'http://www.codeandplay.date/battle-ws/duel/',
   battleMode: false,
   botNumber: 1
 };
