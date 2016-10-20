a76577fd5ccb9eefffc3f016ce57cb7a936f3cb6
AUZANNEAU Benjamin <bauzanneau@emea.msad.sopra>
2016-10-20 16:04:56 +0200

    Last refactoring remove creditentials and url

--- pizzaModules/pizzaConfig.js
+++ pizzaModules/pizzaConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   login: 'pizza.js',
-  mdp: 'PizzaTempo44300Nantes',
+  mdp: '',
   idEquipe: '',
   idPartie: '',
-  battleUrl: 'http://www.codeandplay.date/battle-ws/duel/',
+  battleUrl: 'http://www.---.date/battle-ws/duel/',
   battleMode: false,
   botNumber: 1
 };
