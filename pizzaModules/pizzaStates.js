4730e2c89250c9a64383ae52f4a667307530233e
Benjamin Auzanneau (necraidan) <benjamin.auzanneau.tm@gmail.com>
2016-10-20 17:43:10 +0200

    toto

--- pizzaModules/pizzaStates.js
+++ pizzaModules/pizzaStates.js
@@ -7,9 +7,46 @@ module.exports.gameState = {
 };
 
 module.exports.moveState = {
-  shoot: 'SHOOT',
-  reload: 'RELOAD',
-  cover: 'COVER',
-  aim: 'AIM',
-  bomb: 'BOMB'
+  attack: 'ATTACK',
+  charge: 'CHARGE',
+  cleanse: 'CLEANSE',
+  defend: 'DEFEND',
+  firebolt: 'FIREBOLT',
+  yell: 'YELL',
+  heal: 'HEAL',
+  protect: 'PROTECT',
+  rest: 'REST'
+};
+
+module.exports.playerStatus = {
+  burning: 'BURNING',
+  scared: 'SCARED',
+  stunned: 'STUNNED'
+};
+
+module.exports.players = {
+  archer: {
+    name: 'ARCHER',
+    specialMove: module.exports.moveState.firebolt
+  },
+  chaman: {
+    name: 'CHAMAN',
+    specialMove: module.exports.moveState.cleanse
+  },
+  guard: {
+    name: 'GUARD',
+    specialMove: module.exports.moveState.protect
+  },
+  orc: {
+    name: 'ORC',
+    specialMove: module.exports.moveState.yell
+  },
+  paladin: {
+    name: 'PALADIN',
+    specialMove: module.exports.moveState.charge
+  },
+  prist: {
+    name: 'PRIEST',
+    specialMove: module.exports.moveState.heal
+  }
 };
